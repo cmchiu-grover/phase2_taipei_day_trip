da087958e0e34234a51cd428a078d91807ea588f
Grover Chiu <48580749+cmchiu-grover@users.noreply.github.com>
2025-03-14 14:32:51 +0800

    Modified SQL query and Python function.

--- connection_pool.py
+++ connection_pool.py
@@ -23,46 +23,70 @@ connection_pool = pooling.MySQLConnectionPool(
 def get_connection_pool():
     return connection_pool.get_connection()
 
-def get_attraction_list_rank(page, keyword):
+def get_attraction_list_rank(page: int, keyword: str = ''):
     num_per_page: int = 12
 
-    print("Start...")
+    start_num = (page)*num_per_page
+    
+
+    # print("Start...")
     conn = get_connection_pool()
     cursor = conn.cursor(dictionary=True)
-    cursor.execute(f"""
-                    SELECT 
-                    c.*, 
-                    d.name AS MRT_NAME, 
-                    e.name AS CATEGORY_NAME, 
-                    GROUP_CONCAT(f.url SEPARATOR ',') AS IMAGE_URLS,
-                    CASE 
+    
+    if not keyword:
+        cursor.execute(f"""
+            SELECT c.id, c.name, c.description, 
+            c.address, c.transport, c.rate, 
+            c.lat, c.lng, d.name AS MRT_NAME, 
+            e.name AS CATEGORY_NAME, 
+            GROUP_CONCAT(DISTINCT f.url SEPARATOR ',') AS IMAGE_URLS
+            FROM attraction c 
+            LEFT JOIN mrt d ON c.mrt_id = d.id 
+            LEFT JOIN category e ON c.category_id = e.id 
+            LEFT JOIN images f ON c.id = f.attraction_id
+            GROUP BY c.id, d.name, e.name
+            ORDER BY c.rate DESC, c.id ASC
+            LIMIT {num_per_page} OFFSET {start_num};
+                   """)
+    else:
+        cursor.execute(f"""
+                SELECT c.id, c.name, c.description, 
+                c.address, c.transport, c.rate, 
+                c.lat, c.lng, d.name AS MRT_NAME, 
+                e.name AS CATEGORY_NAME, 
+                GROUP_CONCAT(DISTINCT f.url SEPARATOR ',') AS IMAGE_URLS,
+                CASE 
                     WHEN d.name = '{keyword}' THEN 1   
                     WHEN c.name LIKE '%{keyword}%' THEN 2 
                     ELSE 3                     
-                    END AS sort_order
-                    FROM attraction c 
-                    LEFT JOIN mrt d ON c.mrt_id = d.id 
-                    LEFT JOIN category e ON c.category_id = e.id 
-                    LEFT JOIN images f ON c.id = f.attraction_id 
-                    GROUP BY c.id 
-                    ORDER BY sort_order ASC, c.rate DESC, c.id ASC
-                   """)
+                END AS `RANK`
+                FROM attraction c 
+                LEFT JOIN mrt d ON c.mrt_id = d.id 
+                LEFT JOIN category e ON c.category_id = e.id 
+                LEFT JOIN images f ON c.id = f.attraction_id
+                WHERE d.name = '{keyword}' OR c.name LIKE '%{keyword}%'
+                GROUP BY c.id, d.name, e.name
+                ORDER BY `RANK` ASC, c.rate DESC, c.id ASC
+                LIMIT {num_per_page} OFFSET {start_num};
+                    """)
+    
     results = cursor.fetchall()
 
     cursor.close()
     conn.close()  
 
 
-    print(results)
+    # print(results)
 
-    total_count = len(results)
-
-    next_page = page + 1 if (page + 1) * num_per_page < total_count else None
-
-    start_num = (page)*num_per_page
-    end_num = (page+1)*num_per_page
     
-    return [results[start_num:end_num], next_page]
+
+    next_page = page + 1 if len(results) == num_per_page else None
 
 
+    
+    return [results, next_page]
 
+
+# print(f"Next page is {get_attraction_list_rank(0)[1]}")
+# for i in get_attraction_list_rank(0)[0]:
+#     print(i["name"])
